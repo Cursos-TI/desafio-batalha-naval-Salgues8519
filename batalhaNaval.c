a483bd3e95ac684e2e72a6ecfde7b86adc4a630f
Rafael_Salgues <rafaelsalgues0238@gmail.com>
2025-04-24 18:47:08 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
+#define LINHA 10
+#define COLUNA 10
+
 int main() {
-    int tabuleiro[10][10];
+    int tabuleiro[LINHA][COLUNA];
     int i, j;
-    
+
     // Inicializa todo o tabuleiro com 0 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
             tabuleiro[i][j] = 0;
         }
     }
-    
-    //Posiciona navio vertical 
+
+    // Posiciona navio vertical 
     for (i = 1; i < 1 + 3; i++) {
         tabuleiro[i][2] = 3;
     }
-    
-    //Posiciona navio horizontal 
+
+    // Posiciona navio horizontal 
     for (j = 4; j < 4 + 3; j++) {
         tabuleiro[5][j] = 3;
     }
 
-    //Posiciona navio diagonal 1 (esquerda para direita)
+    // Posiciona navio diagonal 1 (esquerda para direita)
     for (i = 1; i <= 3; i++) {
         for (j = 7; j <= 9; j++) {
             if (i - 1 == j - 7) {
@@ -30,7 +33,7 @@ int main() {
         }
     }
 
-    // Posiciona navio diagonal 2 (direira para esquerda)
+    // Posiciona navio diagonal 2 (direita para esquerda) - seu original
     for ( j = 2; j <  2 + 1; j++){
         for( j = 1; j < 1 + 1; j++){
             for( j = 0; j < 0 + 1; j++){
@@ -41,46 +44,38 @@ int main() {
         tabuleiro[7][j] = 3;
     }
 
+    // Poder em forma de cone com topo em linha 7, coluna 5
+    int centro = 5;
+    int linhaTopo = 7;
+    for (i = 0; i < 3; i++) { // 3 linhas: 7, 8, 9
+        int linha = linhaTopo + i;
+        if (linha >= LINHA) break;
+        for (j = centro - i; j <= centro + i; j++) {
+            if (j >= 0 && j < COLUNA) {
+                if (tabuleiro[linha][j] == 0) {
+                    tabuleiro[linha][j] = 4; // substitui apenas se for água
+                }
+            }
+        }
+    }
+
     printf("Tabuleiro Batalha Naval\n");
 
-    //Imprime cabeçalho de colunas
+    // Imprime cabeçalho de colunas
     printf("   ");
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < COLUNA; j++) {
         printf("%2d ", j);
     }
     printf("\n");
-    
-    // Imprime linha com índice e valores
-    for (i = 0; i < 10; i++) {
+
+    // Imprime tabuleiro
+    for (i = 0; i < LINHA; i++) {
         printf("%2d ", i);
-        for (j = 0; j < 10; j++) {
+        for (j = 0; j < COLUNA; j++) {
             printf(" %d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-    
+
     return 0;
 }
-
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
